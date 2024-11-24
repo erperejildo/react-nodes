b68df9773c43ded4f17088e4f4388ea35b354573
Daniel <d15_1_89@msn.com>
2024-11-24 11:08:09 +0100

    page/Home tests

--- src/__tests__/page.test.tsx
+++ src/__tests__/page.test.tsx
@@ -1,7 +1,31 @@
-import "@testing-library/jest-dom";
+import Home from '@/app/page';
+import { render, screen } from '@testing-library/react';
 
-describe("Page", () => {
-  it("basic test", () => {
-    expect(true).toBe(true);
+jest.mock('../app/components/AutomationBuilder', () => {
+  const MockAutomationBuilder = () => {
+    return <div data-testid="automation-builder">AutomationBuilder Mock</div>;
+  };
+  MockAutomationBuilder.displayName = 'MockAutomationBuilder';
+  return MockAutomationBuilder;
+});
+
+jest.mock('../app/contexts/DnDContext', () => ({
+  DnDProvider: ({ children }: { children: React.ReactNode }) => (
+    <div data-testid="dnd-provider">{children}</div>
+  ),
+}));
+
+describe('Home', () => {
+  it('renders correctly with all components', () => {
+    render(<Home />);
+
+    const mainDiv = screen.getByRole('main', { hidden: true });
+    expect(mainDiv).toHaveClass('main');
+
+    const automationBuilder = screen.getByTestId('automation-builder');
+    expect(automationBuilder).toBeInTheDocument();
+
+    const dndProvider = screen.getByTestId('dnd-provider');
+    expect(dndProvider).toBeInTheDocument();
   });
 });
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,17 @@
-import { ReactFlowProvider } from "@xyflow/react";
-import AutomationBuilder from "./components/AutomationBuilder";
-import { DnDProvider } from "./contexts/DnDContext";
-import styles from "./page.module.css";
+import { ReactFlowProvider } from '@xyflow/react';
+import AutomationBuilder from './components/AutomationBuilder';
+import { DnDProvider } from './contexts/DnDContext';
+import styles from './page.module.css';
 
 const Home = () => {
   return (
-    <div className={styles.main}>
+    <main className={styles.main}>
       <ReactFlowProvider>
         <DnDProvider>
           <AutomationBuilder />
         </DnDProvider>
       </ReactFlowProvider>
-    </div>
+    </main>
   );
 };
 
